541ada72f1830072412b13c7df7237c590aede2c
reuben <creubend@gmail.com>
2022-01-28 14:22:48 -0700

    Add birthday method

---
+++ lib/it_3.rb
@@ -0,0 +1,11 @@
+def math(num)
+  num + 4
+end
+puts math(4)
+
+
+
+def birthday(name, month, day)
+  "Today is the #{day} of #{month}, happy birthday #{name}"
+end
+puts birthday("reuben", "october", 20 )
