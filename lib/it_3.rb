f24dda617c6f69c50df3d16ed554baa3318580c1
reuben <creubend@gmail.com>
2022-01-28 14:33:41 -0700

    Completed it_3.rb

--- lib/it_3.rb
+++ lib/it_3.rb
@@ -9,3 +9,29 @@ def birthday(name, month, day)
   "Today is the #{day} of #{month}, happy birthday #{name}"
 end
 puts birthday("reuben", "october", 20 )
+
+
+
+
+class Dog
+  attr_accessor :name
+
+  def initialize(name, toy, food)
+    @name = name
+    @toy = toy
+    @food = food
+    @happy = true
+    @hungry = true
+  end
+
+  def eat()
+    @hungry = false
+  end
+
+end
+
+
+dog = Dog.new("blitz", "squeaker", "treats")
+
+dog.eat
+p dog
